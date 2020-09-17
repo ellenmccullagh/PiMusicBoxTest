1ea51f58b867b259a4a774d12fc6d38f85d080bd
Ellen McCullagh <ellenmccullagh@gmail.com>
2020-09-16 20:26:22 -0700

    Testing class method as callback

--- buttontest.py
+++ buttontest.py
@@ -3,37 +3,70 @@ import time
 
 import signal
 import sys
-from os import system
+from os import system, path
 
 #BUTTON_GPIO = 16
-BUTTON_PINS = {
-                5: 'moo', #Red
-                6: 'tada', #Blue
-                12: 'tada', #Green
-                13: 'rooster', #Yellow
-                16: 'tada' #White
-                }
-base_path = '~/projects/PiMusicBoxTest/'
+class Button:
+    __init__(self, pin, color, sound):
+        self.pin = pin
+        self.color = color
+        self.sound = sound
+        self.basepath = '~/projects/PiMusicBoxTest/'
+        self.status = True
+
+    def playsound(self):
+        system('aplay -q {}'.format(base_path + self.sound + '.wav'))
+        pass
+
+    def updateStatus(self):
+        self.status = GPIO.input(self.pin)
+        pass
+
+    def reportStatus(self):
+        return self.status
+
+    def reportColor(self):
+        return self.color
+
+BUTTON_PINS = [
+                Button(5, 'Red', 'moo'),
+                Button(5, 'Blue', 'tada'),
+                Button(12, 'Green', 'Tada'),
+                Button(13, 'Yellow', 'rooster'),
+                Button(16, 'White', 'tada')
+                ]
+                # {
+                # 5: 'moo', #Red
+                # 6: 'tada', #Blue
+                # 12: 'tada', #Green
+                # 13: 'rooster', #Yellow
+                # 16: 'tada' #White
+                # }
 
 def signal_handler(sig, frame):
     GPIO.cleanup()
     sys.exit(0)
 
 def button_pressed_callback(channel):
-    for pin in BUTTON_PINS:
-        if GPIO.input(pin) == False:
-            system('aplay -q {}'.format(base_path + BUTTON_PINS[pin] + '.wav'))
-            print("Button {} pressed!".format(pin))
+    for btn in BUTTON_PINS:
+        btn.updateStatus()
+        if btn.reportStatus() == False:
+            btn.playsound()
+            print("{} button pressed".format(self.reportColor()))
             break
+        # if GPIO.input(pin) == False:
+        #     system('aplay -q {}'.format(base_path + BUTTON_PINS[pin] + '.wav'))
+        #     print("Button {} pressed!".format(pin))
+        #     break
 
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
     for pin in BUTTON_PINS:
         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     #GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    for pin in BUTTON_PINS:
-        GPIO.add_event_detect(pin, GPIO.FALLING,
-            callback=button_pressed_callback, bouncetime=100) #i'm not sure how to pass the pin number to the callback function. Maybe I need to embed this in a class somehow
+    for btn in BUTTON_PINS:
+        GPIO.add_event_detect(btn.pin, GPIO.FALLING,
+            callback=btn.playsound, bouncetime=50)  #button_pressed_callback
 
     signal.signal(signal.SIGINT, signal_handler)
     signal.pause()
