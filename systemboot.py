6fa2fb88a13fa955c476be25478e684176301b77
Ellen McCullagh <ellenmccullagh@gmail.com>
2020-09-20 15:29:09 -0700

    bug fix

--- systemboot.py
+++ systemboot.py
@@ -17,6 +17,7 @@ class OnOffButton:
 on_off_btn = OnOffButton(5)
 
 if __name__ == '__main__':
+    GPIO.setmode(GPIO.BCM)
 
     GPIO.setup(on_off_btn.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(on_off_btn.pin, GPIO.FALLING, on_off_btn.onoff, bouncetime=300)
