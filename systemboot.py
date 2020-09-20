e0d50c6c26d03cf81d59543dc3c5948adc09989e
Ellen McCullagh <ellenmccullagh@gmail.com>
2020-09-20 15:30:57 -0700

    bug fix

--- systemboot.py
+++ systemboot.py
@@ -1,6 +1,7 @@
 import buttonhandler as bh
 #from threading import Thread
 import RPi.GPIO as GPIO
+import signal
 
 class OnOffButton:
     def __init__(self, pin):
@@ -21,3 +22,6 @@ if __name__ == '__main__':
 
     GPIO.setup(on_off_btn.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(on_off_btn.pin, GPIO.FALLING, on_off_btn.onoff, bouncetime=300)
+
+    signal.signal(signal.SIGINT, bh.signal_handler)
+    signal.pause()
