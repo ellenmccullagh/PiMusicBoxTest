dfb1f4b3a694669b530e612845e4622d1eee968d
Ellen McCullagh <ellenmccullagh@gmail.com>
2020-09-27 20:14:15 -0700

    updated pin numbers and added GPIO cleanup to test script

--- lighttest.py
+++ lighttest.py
@@ -3,13 +3,13 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-pins = [25, 24, 23, 22, 21]
+pins = [25, 24, 23, 22, 27]
 '''{
 'blue button light':25,
 'yellow button light': 24,
 'green button light': 23,
 'white button light': 22,
-'side leds': 21
+'side leds': 217
 }
 '''
 time.sleep(2)
@@ -22,3 +22,5 @@ for pin in pins:
     GPIO.output(pin, 1)
     time.sleep(3)
     GPIO.output(pin, 0)
+
+GPIO.cleanup()
